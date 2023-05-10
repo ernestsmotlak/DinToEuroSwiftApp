4f459808daa8de4523fc2cc10d902f150c46327b
ernestsmotlak <ernestsmotlak@gmail.com>
2023-05-10 13:43:19 +0200

    Simple app to convert from Dinar to Euro

---
+++ DinToEur/ViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Change font of all buttons on the view controller
+        for button in view.subviews.compactMap({ $0 as? UIButton }) {
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20).withSize(18)
+        }
+    }
+
+    @IBAction func myButton(_ sender: UIButton) {
+        guard let firstValue = Double(field1.text ?? "") else {
+            label1.text = "Invalid input"
+            return
+        }
+        
+        let outputValue = firstValue / 117.68
+        
+        let formattedOutput = String(format: "%.3f", outputValue)
+        
+        label1.text = "\(formattedOutput) €"
+        
+        field1.resignFirstResponder()
+    }
+}
